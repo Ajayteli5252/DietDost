1878b26d8dff279de7f49d7b0779f87f8dfcc07c
Ajayteli5252 <sajaysahu062@gmail.com>
2026-05-16 22:53:01 +0530

    Add global request logger

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ app.use(cors({
 }));
 app.use(express.json());
 
+// Global request logger
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+});
+
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
